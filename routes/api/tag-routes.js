6720b01134e344a5b16d2d42e69a2e29e092af34
alarrabee <alexlarrabee10@gmail.com>
2024-06-01 22:01:19 -0500

    fix tag routes with associated product data to use through option

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }]
+      include: [{ model: Product, through: ProductTag, as: 'tag_productTag' }]
     });
     if (!tagData) {
       res.status(404).json({ message: 'No tag with this id!'});
